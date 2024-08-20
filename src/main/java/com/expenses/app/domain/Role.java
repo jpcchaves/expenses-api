37832859e81a0bef426a8d54bb9e6312f15a952d
jpcchaves <jpcchaves@outlook.com>
2024-08-20 12:46:49 -0300

    add role and user entities

---
+++ src/main/java/com/expenses/app/domain/Role.java
@@ -0,0 +1,62 @@
+package com.expenses.app.domain;
+
+import jakarta.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "roles")
+@SequenceGenerator(name = "seq_role", sequenceName = "seq_role", allocationSize = 1)
+public class Role implements Serializable {
+
+  @Serial private static final long serialVersionUID = -283943394835529005L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_role")
+  private Long id;
+
+  @Column(nullable = false, unique = true, length = 100)
+  private String name;
+
+  public Role() {}
+
+  public Role(Long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Role role = (Role) o;
+    return Objects.equals(id, role.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(id);
+  }
+
+  @Override
+  public String toString() {
+    return "Role{" + "id=" + id + ", name='" + name + '\'' + '}';
+  }
+}
