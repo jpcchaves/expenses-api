c2e7914059e5e389680e9d2337b46f0e0d673151
jpcchaves <jpcchaves@outlook.com>
2024-08-20 11:51:13 -0300

    first commit

---
+++ src/test/java/com/expenses/app/ExpensesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expenses.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
