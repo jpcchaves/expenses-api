9589a31afd8ad179006d1bd1f13c3fcdba215e46
jpcchaves <jpcchaves@outlook.com>
2024-08-20 15:45:27 -0300

    add user repository

---
+++ src/main/java/com/expenses/app/persistence/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.expenses.app.persistence.repository;
+
+import com.expenses.app.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {}
