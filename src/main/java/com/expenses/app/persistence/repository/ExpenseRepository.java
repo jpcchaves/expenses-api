0acb9f8eef32ef5aee6beccacb0ebbf1bbfefbcc
jpcchaves <jpcchaves@outlook.com>
2024-08-21 13:13:33 -0300

    add findExpensesDueInNextFiveDays method

--- src/main/java/com/expenses/app/persistence/repository/ExpenseRepository.java
+++ src/main/java/com/expenses/app/persistence/repository/ExpenseRepository.java
@@ -2,7 +2,19 @@ package com.expenses.app.persistence.repository;
 
 import com.expenses.app.domain.models.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
-public interface ExpenseRepository extends JpaRepository<Expense, Long> {}
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+
+  @Query(
+      value =
+          "SELECT e FROM Expense e WHERE e.dueDate BETWEEN :currentDate AND :nextFiveDays AND e.notificationActive = true")
+  List<Expense> findExpensesDueInNextFiveDays(
+      @Param("currentDate") LocalDate currentDate, @Param("nextFiveDays") LocalDate nextFiveDays);
+}
