df937d5bfd44bee18d37ae7badcea30276ea520a
jpcchaves <jpcchaves@outlook.com>
2024-08-21 17:03:59 -0300

    add order by dueDate asc

--- src/main/java/com/expenses/app/persistence/repository/ExpenseRepository.java
+++ src/main/java/com/expenses/app/persistence/repository/ExpenseRepository.java
@@ -1,20 +1,19 @@
 package com.expenses.app.persistence.repository;
 
 import com.expenses.app.domain.models.Expense;
+import java.time.LocalDate;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
   @Query(
       value =
-          "SELECT e FROM Expense e WHERE e.dueDate BETWEEN :currentDate AND :nextFiveDays AND e.notificationActive = true")
+          "SELECT e FROM Expense e WHERE e.dueDate BETWEEN :currentDate AND :nextFiveDays AND e.notificationActive = true ORDER BY e.dueDate ASC")
   List<Expense> findExpensesDueInNextFiveDays(
       @Param("currentDate") LocalDate currentDate, @Param("nextFiveDays") LocalDate nextFiveDays);
 }
