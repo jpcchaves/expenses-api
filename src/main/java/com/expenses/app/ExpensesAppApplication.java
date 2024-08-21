e6a794541a83b42fcb6ba34c0ae26c80506f6868
jpcchaves <jpcchaves@outlook.com>
2024-08-21 11:44:42 -0300

    enable scheduling and async

--- src/main/java/com/expenses/app/ExpensesAppApplication.java
+++ src/main/java/com/expenses/app/ExpensesAppApplication.java
@@ -2,12 +2,15 @@ package com.expenses.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class ExpensesAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExpensesAppApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ExpensesAppApplication.class, args);
+  }
 }
