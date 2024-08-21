8c0863f4d95fa1d63657125fc842e86401d7a1a7
jpcchaves <jpcchaves@outlook.com>
2024-08-21 11:20:05 -0300

    add update user test and add assertion in both post and put tests

--- src/test/java/com/expenses/app/controller/UserControllerTest.java
+++ src/test/java/com/expenses/app/controller/UserControllerTest.java
@@ -7,6 +7,7 @@ import com.expenses.app.config.AbstractTestContainerConfig;
 import com.expenses.app.config.TestsConfigConstants;
 import com.expenses.app.domain.dto.user.UserRequestDTO;
 import com.expenses.app.domain.dto.user.UserResponseDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -18,9 +19,9 @@ import io.restassured.specification.RequestSpecification;
 import java.io.IOException;
 import net.datafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -33,20 +34,11 @@ class UserControllerTest extends AbstractTestContainerConfig {
   private static String TEST_PASSWORD;
   private static RequestSpecification requestSpecification;
   private static UserRequestDTO userRequestDTO;
-  @Autowired private UserController userController;
 
   @BeforeAll
   static void setup() {
 
     faker = new Faker();
-
-    TEST_EMAIL = faker.internet().emailAddress();
-
-    TEST_NAME = faker.name().fullName();
-
-    TEST_PASSWORD = faker.number().digits(12);
-
-    userRequestDTO = new UserRequestDTO(TEST_NAME, TEST_EMAIL, TEST_PASSWORD, TEST_PASSWORD);
 
     requestSpecification =
         new RequestSpecBuilder()
@@ -59,6 +51,18 @@ class UserControllerTest extends AbstractTestContainerConfig {
     mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
     mapper.registerModule(new JavaTimeModule());
+  }
+
+  @BeforeEach
+  void setupEach() {
+
+    TEST_EMAIL = faker.internet().emailAddress();
+
+    TEST_NAME = faker.name().fullName();
+
+    TEST_PASSWORD = faker.number().digits(12);
+
+    userRequestDTO = new UserRequestDTO(TEST_NAME, TEST_EMAIL, TEST_PASSWORD, TEST_PASSWORD);
   }
 
   @DisplayName("Integration test given user request dto when save should return user response DTO")
@@ -81,5 +85,53 @@ class UserControllerTest extends AbstractTestContainerConfig {
     UserResponseDTO responseDTO = mapper.readValue(response, UserResponseDTO.class);
 
     assertNotNull(responseDTO);
+
+    assertTrue(responseDTO.getId() > 0);
+
+    assertEquals(userRequestDTO.getName(), responseDTO.getName());
+    assertEquals(userRequestDTO.getEmail(), responseDTO.getEmail());
+  }
+
+  @DisplayName(
+      "Given a User ID and User Request DTO when updating the user it should return a User Response DTO")
+  @Test
+  void integrationTestGivenUserIdAndUserRequestDTO_WhenUpdateUser_ShouldReturnUserResponseDTO()
+      throws JsonProcessingException {
+
+    String postResponse =
+        given()
+            .spec(requestSpecification)
+            .contentType(TestsConfigConstants.CONTENT_TYPE_JSON)
+            .body(userRequestDTO)
+            .when()
+            .post()
+            .then()
+            .extract()
+            .body()
+            .asString();
+
+    UserResponseDTO responseDTO = mapper.readValue(postResponse, UserResponseDTO.class);
+
+    UserRequestDTO updateRequestDTO =
+        new UserRequestDTO("Updated Name", "updatedemail@test.com", "123456", "123456");
+
+    String putResponse =
+        given()
+            .spec(requestSpecification)
+            .contentType(TestsConfigConstants.CONTENT_TYPE_JSON)
+            .body(updateRequestDTO)
+            .when()
+            .put("/{userId}", responseDTO.getId())
+            .then()
+            .extract()
+            .body()
+            .asString();
+
+    UserResponseDTO updateResponseDTO = mapper.readValue(putResponse, UserResponseDTO.class);
+
+    assertNotNull(updateResponseDTO);
+
+    assertEquals(updateRequestDTO.getName(), updateResponseDTO.getName());
+    assertEquals(updateRequestDTO.getEmail(), updateResponseDTO.getEmail());
   }
 }
