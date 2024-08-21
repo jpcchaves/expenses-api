2084a9e7732ed866f2d3a35719770dd7ad1ae9fb
jpcchaves <jpcchaves@outlook.com>
2024-08-21 13:13:16 -0300

    add find by name method

--- src/main/java/com/expenses/app/persistence/repository/ExpenseSourceRepository.java
+++ src/main/java/com/expenses/app/persistence/repository/ExpenseSourceRepository.java
@@ -1,8 +1,14 @@
 package com.expenses.app.persistence.repository;
 
 import com.expenses.app.domain.models.ExpenseSource;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ExpenseSourceRepository extends JpaRepository<ExpenseSource, Long> {}
+public interface ExpenseSourceRepository extends JpaRepository<ExpenseSource, Long> {
+
+  @Query(value = "SELECT * FROM expense_sources u WHERE u.name = :name", nativeQuery = true)
+  Optional<ExpenseSource> findByName(String name);
+}
