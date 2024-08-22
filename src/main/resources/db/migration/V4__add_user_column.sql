81c14a3fea5075e11dcc4e82fe3d9a554507af06
jpcchaves <jpcchaves@outlook.com>
2024-08-22 09:19:38 -0300

    add constraint user_fk

--- src/main/resources/db/migration/V4__add_user_column.sql
+++ src/main/resources/db/migration/V4__add_user_column.sql
@@ -6,3 +6,8 @@ SET user_id = 1;
 
 ALTER TABLE expense_sources
 ALTER COLUMN user_id SET NOT NULL;
+
+alter table if exists expense_sources
+       add constraint user_fk
+       foreign key (user_id)
+       references users;
