174cd7ec088c6318b592b5ec13a39266a3b0d94b
jpcchaves <jpcchaves@outlook.com>
2024-08-22 08:31:31 -0300

    add findByName test

---
+++ src/test/java/com/expenses/app/persistence/repository/ExpenseSourceRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.expenses.app.persistence.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.expenses.app.config.AbstractTestContainerConfig;
+import com.expenses.app.domain.models.ExpenseSource;
+import com.expenses.app.domain.models.User;
+import java.util.Optional;
+import net.datafaker.Faker;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class ExpenseSourceRepositoryTest extends AbstractTestContainerConfig {
+
+  @Autowired private ExpenseSourceRepository expenseSourceRepository;
+
+  @Autowired private UserRepository userRepository;
+
+  private Faker faker;
+
+  private User user;
+
+  private ExpenseSource expenseSource;
+
+  private String expenseSourceName;
+
+  @BeforeEach
+  void setup() {
+
+    faker = new Faker();
+
+    expenseSourceName = faker.lorem().characters(10);
+
+    user = userRepository.findByEmail("test@test.com").get();
+
+    expenseSource =
+        expenseSourceRepository.saveAndFlush(new ExpenseSource(expenseSourceName, user));
+  }
+
+  @DisplayName("Test given userId and Name when findByName then should return ExpenseSource object")
+  @Test
+  void testGivenUserIdAndName_WhenFindByName_ThenShouldReturnExpenseSourceObject() {
+
+    // Given / When
+    Optional<ExpenseSource> expenseSource =
+        expenseSourceRepository.findByName(user.getId(), expenseSourceName);
+
+    // Then
+    assertNotNull(expenseSource.get());
+    assertEquals(expenseSourceName, expenseSource.get().getName());
+    assertEquals(user.getId(), expenseSource.get().getUser().getId());
+  }
+}
