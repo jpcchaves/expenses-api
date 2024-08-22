08ab690ce16a624ce4f679dbe60294567925241a
jpcchaves <jpcchaves@outlook.com>
2024-08-22 08:44:40 -0300

    add add findAll test

--- src/test/java/com/expenses/app/persistence/repository/ExpenseSourceRepositoryTest.java
+++ src/test/java/com/expenses/app/persistence/repository/ExpenseSourceRepositoryTest.java
@@ -13,6 +13,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -55,5 +59,21 @@ class ExpenseSourceRepositoryTest extends AbstractTestContainerConfig {
     assertNotNull(expenseSource.get());
     assertEquals(expenseSourceName, expenseSource.get().getName());
     assertEquals(user.getId(), expenseSource.get().getUser().getId());
+  }
+
+  @DisplayName("Test given userId and Pageable when find all should return Page of ExpenseSource")
+  @Test
+  void testGivenUserIdAndPageableWhenFindAllShouldReturnPageObject() {
+
+    // Given
+    Pageable pageable = PageRequest.of(0, 20, Sort.by(Sort.Direction.ASC, "created_at"));
+
+    // When
+    Page<ExpenseSource> expenseSourcePage = expenseSourceRepository.findAll(user.getId(), pageable);
+
+    // Then
+    assertNotNull(expenseSourcePage);
+
+    assertEquals(2, expenseSourcePage.getContent().size());
   }
 }
