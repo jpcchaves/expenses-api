2ea46c61fe1965a0d8ba23f29c3af5dfd200a8f1
jpcchaves <jpcchaves@outlook.com>
2024-08-26 09:21:49 -0300

    allow swagger as public

--- src/main/java/com/expenses/app/security/RequestMatchersConstants.java
+++ src/main/java/com/expenses/app/security/RequestMatchersConstants.java
@@ -3,5 +3,11 @@ package com.expenses.app.security;
 public class RequestMatchersConstants {
 
   public static final String[] PUBLIC_REQUEST_MATCHERS =
-      new String[] {"/swagger-ui/**", "/api/v1/auth/login", "/api/v1/auth/register"};
+      new String[] {
+        "/swagger-ui/**",
+        "/v3/api-docs/**",
+        "/swagger-ui.html",
+        "/api/v1/auth/login",
+        "/api/v1/auth/register"
+      };
 }
