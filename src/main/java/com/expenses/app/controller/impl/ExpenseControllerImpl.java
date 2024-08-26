daa7c31abc1d343c4096b1d45545f128f9ebe8aa
jpcchaves <jpcchaves@outlook.com>
2024-08-26 11:26:07 -0300

    add expense controller swagger doc

--- src/main/java/com/expenses/app/controller/impl/ExpenseControllerImpl.java
+++ src/main/java/com/expenses/app/controller/impl/ExpenseControllerImpl.java
@@ -1,5 +1,6 @@
 package com.expenses.app.controller.impl;
 
+import com.expenses.app.controller.ExpenseController;
 import com.expenses.app.domain.dto.common.PaginationResponseDTO;
 import com.expenses.app.domain.dto.expense.ExpenseRequestDTO;
 import com.expenses.app.domain.dto.expense.ExpenseResponseDTO;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/expenses")
-public class ExpenseControllerImpl {
+public class ExpenseControllerImpl implements ExpenseController {
 
   private final ExpenseService expenseService;
 
@@ -20,6 +21,7 @@ public class ExpenseControllerImpl {
     this.expenseService = expenseService;
   }
 
+  @Override
   @PostMapping
   public ResponseEntity<ExpenseResponseDTO> create(
       @Valid @RequestBody ExpenseRequestDTO requestDTO) {
@@ -27,6 +29,7 @@ public class ExpenseControllerImpl {
     return ResponseEntity.status(HttpStatus.CREATED).body(expenseService.create(requestDTO));
   }
 
+  @Override
   @PutMapping("/{expenseId}")
   public ResponseEntity<ExpenseResponseDTO> update(
       @PathVariable(name = "expenseId") Long expenseId,
@@ -35,12 +38,14 @@ public class ExpenseControllerImpl {
     return ResponseEntity.ok(expenseService.update(expenseId, requestDTO));
   }
 
+  @Override
   @GetMapping
   public ResponseEntity<PaginationResponseDTO<ExpenseResponseDTO>> list(Pageable pageable) {
 
     return ResponseEntity.ok(expenseService.list(pageable));
   }
 
+  @Override
   @GetMapping("/{expenseId}")
   public ResponseEntity<ExpenseResponseDTO> findById(
       @PathVariable(name = "expenseId") Long expenseId) {
